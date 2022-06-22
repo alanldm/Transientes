279c766189484526ce4abe7fd19e85a1150536a5
Alan <alanlimamedeiross@gmail.com>
2022-06-22 18:35:20 -0300

    Adicionando as funções para os cálculos das tensões

--- calc.py
+++ calc.py
@@ -1,6 +1,11 @@
-def tl(zl, z0):
+def t_l(zl, z0):
     return (zl-z0)/(zl+z0)
 
-def ts(zs, z0):
+def t_s(zs, z0):
     return (zs-z0)/(zs+z0)
     
+def v_0(vs, z0, zs):
+    return (z0*vs)/(z0+zs)
+
+def v_l(vs, z0, zl):
+    return (zl*vs)/(zl+z0)
